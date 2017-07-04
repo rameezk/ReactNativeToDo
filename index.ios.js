88960f7197920d2e35204b39f215041d24b5f751
Rameez Khan <rameezkhan.sa@gmail.com>
2017-07-04 19:01:38 +0200

    Display hello world

--- index.ios.js
+++ index.ios.js
@@ -1,40 +1,10 @@
-import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import { AppRegistry } from 'react-native'
 
-export default class ReactNativeTodo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{"\n"}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+import App from './app'
+
+const ReactNativeTodo = () => {
+  return <App />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
-});
-
-AppRegistry.registerComponent("ReactNativeTodo", () => ReactNativeTodo);
+AppRegistry.registerComponent('ReactNativeTodo', () => ReactNativeTodo)
