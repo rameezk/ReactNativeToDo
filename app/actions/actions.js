4a48cf9e931fb9de3e28a6e387402aae03046688
Rameez Khan <rameezkhan.sa@gmail.com>
2017-07-06 16:08:37 +0200

    Add actions

---
+++ app/actions/actions.js
@@ -0,0 +1,25 @@
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from './constants'
+
+let nextTodoId = 0
+
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    id: nextTodoId++,
+    text,
+  }
+}
+
+export function toggleTodo(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter,
+  }
+}
